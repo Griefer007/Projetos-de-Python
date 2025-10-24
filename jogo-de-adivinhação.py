a51e451cab1151e34c2f3db73e26de8ec4e3ac55
Griefer007 <caiosantanabarbosa@gmail.com>
2025-10-24 16:30:29 -0300

    Create jogo-de-adivinhação.py
    um jogo legal de adivinhação

---
+++ jogo-de-adivinhação.py
@@ -0,0 +1,39 @@
+import discord
+import random
+import asyncio
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('$guess'):
+            await message.channel.send('Adivinhe um número entre 1 a 10.')
+
+            def is_correct(m):
+                return m.author == message.author and m.content.isdigit()
+
+            answer = random.randint(1, 10)
+
+            try:
+                guess = await self.wait_for('message', check=is_correct, timeout=5.0)
+            except asyncio.TimeoutError:
+                return await message.channel.send(f'Desculpe, você demorou. A resposta era {answer}.')
+
+            if int(guess.content) == answer:
+                await message.channel.send('Acerou em cheio!')
+            else:
+                await message.channel.send(f'Você errou, e a resposta era {answer}.')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('SEU TOKEN AQUI')
